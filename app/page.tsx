5d75c8602d80a49371ea53a5d1134aab7cb87ffa
22520592 <22520592@gm.uit.edu.vn>
2024-07-27 17:27:46 +0700

    add auth

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import "../styles/theme.css";
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-dark100_light500 font-josefin">Huynh toi ne</h1>
-    </div>
-  );
-}
